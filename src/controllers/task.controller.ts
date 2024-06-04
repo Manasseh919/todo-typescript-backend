1d4224db7a171add27446b1c4053b156b784537d
Manasseh919 <69004938+Manasseh919@users.noreply.github.com>
2024-06-04 22:39:44 +0200

    edit task delete task

--- src/controllers/task.controller.ts
+++ src/controllers/task.controller.ts
@@ -74,6 +74,25 @@ export const getAllComletedTask = async (
     throw error;
   }
 };
+export const getTasksForToday = async (
+  request: AuthRequest,
+  response: Response
+) => {
+  try {
+    const userId = request.user;
+    const todaysISODate = new Date();
+    todaysISODate.setHours(0, 0, 0, 0);
+    const tasks = await Task.find({
+      user: userId,
+      date: todaysISODate.toISOString(),
+    });
+    response.send(tasks);
+  } catch (error) {
+    console.log("error in getTasksForToday", error);
+    response.send({ error: "Error while fetching tasks" });
+    throw error;
+  }
+};
 
 export const toggleTaskStatus = async (
   request: AuthRequest,
@@ -96,3 +115,40 @@ export const toggleTaskStatus = async (
     throw error;
   }
 };
+
+export const deleteTask = async (request: AuthRequest, response: Response) => {
+  try {
+    const { id } = request.params
+    await Task.deleteOne({
+      _id: id,
+    })
+    response.send({ message: "Task deleted" })
+  } catch (error) {
+    console.log("error in deleteTask", error)
+    response.send({ error: "Error while deleting task" })
+    throw error
+  }
+}
+
+export const editTask = async (request: AuthRequest, response: Response) => {
+  try {
+    const { _id, categoryId, date, name }: ITask = request.body
+    await Task.updateOne(
+      {
+        _id,
+      },
+      {
+        $set: {
+          name,
+          categoryId,
+          date,
+        },
+      }
+    )
+    response.send({ message: "Task updated successfully" })
+  } catch (error) {
+    console.log("error in editTask", error)
+    response.send({ error: " Error while updating the task" })
+    throw error
+  }
+}
--- src/routes/task.route.ts
+++ src/routes/task.route.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { authenticationMiddleware } from "../middleware";
-import { createTask, getAllComletedTask, getAllTask, getAllTasksByCategory, toggleTaskStatus } from "../controllers/task.controller";
+import { createTask, getAllComletedTask, getAllTask, getAllTasksByCategory, getTasksForToday, toggleTaskStatus } from "../controllers/task.controller";
 
 
 const taskRoutes = express.Router()
@@ -10,6 +10,7 @@ taskRoutes.use(authenticationMiddleware)
 taskRoutes.route("/").get(getAllTask)
 taskRoutes.route("/tasks-by-categories/:id").get(getAllTasksByCategory)
 taskRoutes.route("/completed").get(getAllComletedTask)
+taskRoutes.route("/today").get(getTasksForToday)
 taskRoutes.route("/create").post(createTask)
 taskRoutes.route("/update/:id").put(toggleTaskStatus)
 
